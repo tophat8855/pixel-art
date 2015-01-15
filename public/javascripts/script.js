c5ca11d3627975384b7d1c8087d4e77959be314a
tophat8855 <hrfarley@gmail.com>
2015-01-15 14:59:40 -0800

    Colors! So pretty

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,11 +1,12 @@
 var canvas = document.getElementsByTagName("body")[0];
 var penColor = "rgb(255,0,0)";
 
-
 function initializeGrid(height, width, pixSize) {
   for (var y = 0; y < height; y++) {
     for (var x = 0; x < width; x++) {
       var pixel = document.createElement("BUTTON");
+      pixel.style.height = "20px";
+      pixel.style.width = "20px";
       pixel.addEventListener("click", function() {
         this.style.background = penColor;
       })
@@ -16,4 +17,42 @@ function initializeGrid(height, width, pixSize) {
   };
 };
 
-initializeGrid(24, 30, 20);
+function initializeColorRow(pixSize){
+  paintWithAllTheColorsOfTheWind("white", "Eraser");
+  paintWithAllTheColorsOfTheWind("cyan");
+  paintWithAllTheColorsOfTheWind("magenta");
+  paintWithAllTheColorsOfTheWind("navy");
+  paintWithAllTheColorsOfTheWind("pink");
+  paintWithAllTheColorsOfTheWind("yellow");
+  paintWithAllTheColorsOfTheWind("green");
+  paintWithAllTheColorsOfTheWind("black");
+  paintWithAllTheColorsOfTheWind("red");
+  paintWithAllTheColorsOfTheWind("orange");
+  paintWithAllTheColorsOfTheWind("brown");
+  paintWithAllTheColorsOfTheWind("blue");
+
+};
+
+function paintWithAllTheColorsOfTheWind(colorName, label){
+  if (label === undefined) {
+    label = colorName;
+  };
+
+  var color = document.createElement("button");
+  color.style.background = colorName;
+  color.style.width = "60px";
+  color.style.height = "40px";
+  color.innerText = label;
+  color.addEventListener("click", function() {
+    penColor = colorName;
+  });
+  canvas.appendChild(color);
+}
+
+function setPenColor(color) {
+  penColor = color;
+  canvas.style.background = color;
+}
+
+initializeGrid(24, 30, 50);
+initializeColorRow(20);
