064b2494c2a86bcfcbd5f6c5a0e3f725c7d9cb0c
tophat8855 <hrfarley@gmail.com>
2015-01-15 14:25:15 -0800

    took out canvas. do buttons insteaad

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,13 +1,19 @@
-var board = document.getElementsByTagName("canvas")[0];
+var canvas = document.getElementsByTagName("body")[0];
+var penColor = "rgb(255,0,0)";
 
-var context2D = board.getContext("2d");
 
-for (var y = 0; y < 24; y++) {
-  for (var x = 0; x < 30; x++) {
-    printPixel(x*20, y*20);
+function initializeGrid(height, width, pixSize) {
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      var pixel = document.createElement("BUTTON");
+      pixel.addEventListener("click", function() {
+        this.style.background = penColor;
+      })
+      canvas.appendChild(pixel)
+    };
+    var new_line= document.createElement("DIV");
+    canvas.appendChild(new_line);
   };
 };
 
-function printPixel(x,y) {
-  context2D.strokeRect(x, y, 20, 20);
-};
+initializeGrid(24, 30, 20);
