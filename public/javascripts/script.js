1e8a7d28b877e32a6718abe447513c3646a8be5e
tophat8855 <hrfarley@gmail.com>
2015-01-15 13:09:24 -0800

    made board

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,13 @@
-function() {
+var board = document.getElementsByTagName("canvas")[0];
 
+var context2D = board.getContext("2d");
+
+for (var y = 0; y < 24; y++) {
+  for (var x = 0; x < 30; x++) {
+    printPixel(x*20, y*20);
+  };
+};
+
+function printPixel(x,y) {
+  context2D.strokeRect(x, y, 20, 20);
 };
